b2fcbbb65cfd29749237d67117ec27b8089d5f68
ChangWynn <huynhchang.one@gmail.com>
2023-04-19 00:38:41 +0100

    TDD AccountModel transaction property

--- test/accountModel.test.js
+++ test/accountModel.test.js
@@ -11,6 +11,8 @@ describe(AccountModel, () => {
     it('should have a balance property initially set to 0', () => {
       expect(model.balance).toEqual(0)
     })
+    it('should have a transactions property initially set to an empty', () => {
+      expect(model.transactions).toEqual([])
+    })
   })
- 
 })
