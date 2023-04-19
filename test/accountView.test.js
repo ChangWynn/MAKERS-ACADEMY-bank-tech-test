b43c3c26f255f57483e2bba13a214605a0544b51
ChangWynn <huynhchang.one@gmail.com>
2023-04-19 16:14:30 +0100

    TDD printStatement displays debit in a two decimals format

--- test/accountView.test.js
+++ test/accountView.test.js
@@ -11,13 +11,11 @@ describe(AccountView, () => {
       credit: 100,
       debit: 0
     }
-    
     debitTransaction = {
       date: new Date(2023, 3, 19),
       credit: 0,
       debit: 100
     }
-    
     view = new AccountView()
     consoleSpy = jest.spyOn(console, 'log')
   })
@@ -27,38 +25,67 @@ describe(AccountView, () => {
   })
 
   describe(".printStatement", () => {
-    it("should display a date in the format DD/MM/YYYY", () => {
-      model = {
-        balance: 0,
-        transactions: [creditTransaction]
-      }
 
-      const formattedDate = "19/04/2023"
-    
-      view.printStatement(model)
-      expect(consoleSpy.mock.calls[0][0]).toEqual(expect.stringContaining(formattedDate))
+    describe("date output", () => {
+      it("should be in the format DD/MM/YYYY", () => {
+        model = {
+          balance: 0,
+          transactions: [creditTransaction]
+        }
+
+        const formattedDate = "19/04/2023"
+      
+        view.printStatement(model)
+        expect(consoleSpy.mock.calls[0][0]).toEqual(expect.stringContaining(formattedDate))
+      })
     })
 
-    it("should display credit in a two decimal format", () => {
-      const model = {
-        balance: 0,
-        transactions: [creditTransaction]
-      }
-      const formattedCredit = "100.00"
-    
-      view.printStatement(model)
-      expect(consoleSpy.mock.calls[0][0]).toEqual(expect.stringContaining(formattedCredit))
-    })
+    describe("credit output", () => {
+      it("should be displayed in a two decimals format", () => {
+        const model = {
+          balance: 0,
+          transactions: [creditTransaction]
+        }
+        const formattedCredit = "100.00"
+      
+        view.printStatement(model)
+        expect(consoleSpy.mock.calls[0][0]).toEqual(expect.stringContaining(formattedCredit))
+      })
 
-    it("should not display anything when credit is 0", () => {
-      const model = {
-        balance: 0,
-        transactions: [debitTransaction]
-      }
-      const partialExpectedOutput = "19/04/2023 ||  ||"
+      it("should not display anything when it's equal to 0", () => {
+        const model = {
+          balance: 0,
+          transactions: [debitTransaction]
+        }
+        const partialExpectedOutput = "19/04/2023 ||  ||"
+      
+        view.printStatement(model)
+        expect(consoleSpy.mock.calls[0][0]).toEqual(expect.stringContaining(partialExpectedOutput))
+      })
+    })
     
-      view.printStatement(model)
-      expect(consoleSpy.mock.calls[0][0]).toEqual(expect.stringContaining(partialExpectedOutput))
+    describe("debit output", () => {
+      it("should be displayed in a two decimals format", () => {
+        const model = {
+          balance: 0,
+          transactions: [debitTransaction]
+        }
+        const formattedDebit = "19/04/2023 ||  || 100.00"
+      
+        view.printStatement(model)
+        expect(consoleSpy.mock.calls[0][0]).toEqual(expect.stringContaining(formattedDebit))
+      })
+
+      it("should not display anything when it's equal to 0", () => {
+        const model = {
+          balance: 0,
+          transactions: [creditTransaction]
+        }
+        const partialExpectedOutput = "19/04/2023 || 100.00 ||  ||"
+
+        view.printStatement(model)
+        expect(consoleSpy.mock.calls[0][0]).toEqual(expect.stringContaining(partialExpectedOutput))
+      })
     })
   })
 })
