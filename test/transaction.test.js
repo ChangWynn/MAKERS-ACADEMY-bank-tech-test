d34ab85a4b5d2f39a61a0407e41a154a2de90bb6
ChangWynn <huynhchang.one@gmail.com>
2023-04-19 11:24:03 +0100

    minor refactoring

--- test/transaction.test.js
+++ test/transaction.test.js
@@ -1,4 +1,4 @@
-const Transaction = require('../lib/transaction')
+const Transaction = require("../lib/transaction")
 
 describe(Transaction, () => {
 
@@ -7,23 +7,23 @@ describe(Transaction, () => {
     transaction = new Transaction()
   })
 
-  describe('new instance', () => {
+  describe("new instance", () => {
 
-    it('should have its properties set as private', () => {
+    it("should have its properties set as private", () => {
       const properties = Object.getOwnPropertyNames(transaction)
-      expect(properties.includes('date')).toBe(false)
-      expect(properties.includes('credit')).toBe(false)
-      expect(properties.includes('debit')).toBe(false)
+      expect(properties.includes("date")).toBe(false)
+      expect(properties.includes("credit")).toBe(false)
+      expect(properties.includes("debit")).toBe(false)
     })
 
-    it('should have credit and debit property initially set to 0', () => {
+    it("should have credit and debit property initially set to 0", () => {
       expect(transaction.credit).toEqual(0)
       expect(transaction.debit).toEqual(0)
     })
-    
-    it('should have a date property holding the date of instanciation', () => {
+
+    it("should have a date property holding the date of instanciation", () => {
       const now = new Date(2023, 3, 18) 
-      const fakeDateNow = jest.spyOn(Date, 'now')
+      const fakeDateNow = jest.spyOn(Date, "now")
       fakeDateNow.mockImplementation(() => now )
   
       const mockedTransaction = new Transaction()
@@ -31,15 +31,15 @@ describe(Transaction, () => {
     })
   })
 
-  describe('.credit setter method', () => {
-    it('should assign the new value to the credit property', () => {
+  describe(".credit setter method", () => {
+    it("should assign the new value to the credit property", () => {
       transaction.credit = 100
       expect(transaction.credit).toEqual(100)
     })
   })
 
-  describe('.debit setter method', () => {
-    it('should assign the new value to the debit property', () => {
+  describe(".debit setter method", () => {
+    it("should assign the new value to the debit property", () => {
       transaction.debit = 100
       expect(transaction.debit).toEqual(100)
     })
