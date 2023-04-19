373a62751c1ee83130bd70fc0fe6d2978b8491a2
ChangWynn <huynhchang.one@gmail.com>
2023-04-19 15:13:43 +0100

    TDD AccountView's print statement displays credits in a decimal format

--- test/accountView.test.js
+++ test/accountView.test.js
@@ -3,9 +3,16 @@ const AccountView = require("../lib/accountView")
 describe(AccountView, () => {
 
   let view;
-  let consoleSpy
+  let creditTransaction;
+  let consoleSpy;
   beforeEach(() => {
     view = new AccountView()
+    creditTransaction = {
+      date: new Date(2023, 3, 19),
+      credit: 100,
+      debit: 0
+    }
+  
     consoleSpy = jest.spyOn(console, 'log')
   })
 
@@ -15,21 +22,26 @@ describe(AccountView, () => {
 
   describe(".printStatement", () => {
     it("should display a date in the format DD/MM/YYYY", () => {
-      const transaction = {
-        date: new Date(2023, 3, 19),
-        credit: 100,
-        debit: 0
+      model = {
+        balance: 0,
+        transactions: [creditTransaction]
       }
 
-      const model = {
-        balance: 0,
-        transactions: [transaction]
-      }
-      
       const formattedDate = "19/04/2023"
     
       view.printStatement(model)
       expect(consoleSpy.mock.calls[0][0]).toEqual(expect.stringContaining(formattedDate))
+    })
+
+    it("should display credit in a two decimal format", () => {
+      const model = {
+        balance: 0,
+        transactions: [creditTransaction]
+      }
+      const formattedCredit = "100.00"
+    
+      view.printStatement(model)
+      expect(consoleSpy.mock.calls[0][0]).toEqual(expect.stringContaining(formattedCredit))
     })
   })
 })
